6919ac9c0126bf1ffa3362a52eff4003a244b49d
ShpatzR <roni1kohen@gmail.com>
2024-07-10 17:25:36 +0100

    sinin form

--- main.py
+++ main.py
@@ -1,19 +1,78 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+import secrets
+
+secret_key = secrets.token_hex(16)
 
 
 
+users =[{
+    "name": "roni", 
+    "password": "1234", 
+    "color": "#206A5D"
+    }]
+
+colors = ["#206A5D", "#81B214", "#FFCC29", "#F58634"]
 
 app = Flask(__name__)
 
+app.secret_key = secret_key
+
+def checkUser(username, password):
+    for name in users:
+        if name["name"] == username:
+            if name["password"] == password:
+                return True 
+            else: return False
+    else: return False 
+
+
+
 @app.route("/")
+def index():
+    return redirect(url_for("login"))
+
+@app.route("/log-in", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        name = request.form.get("username")
+        password = request.form.get("password")
+        print(name, password)
+        if checkUser(username=name, password=password):
+            session['username'] = name
+            session['profile'] = name[0]
+            return redirect(url_for("home"))
+        else: return "Invalid credentials", 401 
     return render_template("login.html")
 
-@app.route("/sign-in")
-def sign_in():
-    return render_template("sign.html")
+@app.route("/sign-in",  methods=["GET", "POST"])
+def signin():
+    if request.method == "POST":
+        name = request.form.get("username")
+        password = request.form.get("password")
+        password2 = request.form.get("password-2")
+        # color = request.form.get("")
+        if password == password2:
+            new_user = {"name": name, "password": password}
+            users.append(new_user)
+            print(users)
+            return redirect(url_for('home'))
+        else: 
+            return render_template('sign.html', password2_error=True)
+      
+    return render_template('sign.html')
 
+@app.route("/home")
+def home():
+    if 'username' in session:
+        username = session["username"]
+        profile = session['profile']
+        if profile.isalpha():
+            profile = profile.upper()
+        return render_template("home.html", username = username, profile = profile)
+    else: 
+        return redirect(url_for("login"))
 
+  
 
 
 
---
+++ static/js/login.js
@@ -0,0 +1,30 @@
+const btnShow = document.getElementById("show-password");
+const inputPassword = document.getElementById("password");
+const inputPassword2 = document.getElementById("password-2");
+
+
+
+console.log(sinInColor)
+
+function toggleShow(){
+    if (inputPassword2) {
+        if (inputPassword.type == "password") {
+            inputPassword.type = "text";
+            inputPassword2.type = "text";  
+        } else {
+            inputPassword.type = "password";
+            inputPassword2.type = "password";  
+        }
+    } else {
+        if (inputPassword.type == "password" ) {
+            inputPassword.type = "text";
+        } else {
+            inputPassword.type = "password";
+        }
+        
+    }
+}
+
+
+btnShow.addEventListener("click", toggleShow)
+
